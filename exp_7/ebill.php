225f355f16c32bfc6c0ee65b43dd119b2b14c8b8
Paxlows <arunbabuvn@gmail.com>
2021-12-18 12:34:58 +0530

    exp7

---
+++ exp_7/ebill.php
@@ -0,0 +1,21 @@
+<?php 
+	$ename=$_POST["ename"];
+	$bpay=$_POST["bpay"];
+	$hra=$_POST["hra"];
+	$da=$_POST["da"];
+	$percentage=($bpay * $da)/100;
+	$total=$bpay + $percentage + $hra;
+	$out = "-- Employee Details --\n
+    Employee Name: $ename\n
+    Basic Salary: $bpay\n
+    DA in %: $da\n
+    HRA: $hra\n
+    Total: $total\n
+    ";
+	echo $out;
+	$filename = "temp.txt"
+	$file = fopen($filename, "w+");
+	fwrite($file, $out);
+	fclose($file);
+	header("Location:$filename");
+?>
